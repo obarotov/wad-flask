6051ae78e346bc199d26bdffcd284e334ba5ece3
otabek <otabekbarotov15@gmail.com>
2026-04-07 11:38:00 +0500

    routes.py updated

--- app/routes.py
+++ app/routes.py
@@ -1,18 +1,21 @@
 from flask import render_template
 from app import app
+from app.forms import LoginForm
 
-@app.route('/')
-@app.route('/index')
-def index():
-    user = {'username': 'Otabek'}
-    posts = [
-        {
-            'author': {'username': 'Abdulaziz'},
-            'body': 'Beautiful day in Tajikistan!'
-        },
-        {
-            'author': {'username': 'Ahmadjon'},
-            'body': 'The Invincible series was so cool!'
-        }
-    ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+# ...
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
+
+from flask import render_template, flash, redirect
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)
